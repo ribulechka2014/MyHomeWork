745ce1bb4d3ac6a9619f9ff94abb07c5dd541ded
Alina Zakharova <alusya2014@gmail.com>
2022-09-18 22:35:24 +0300

    My lesson 3 added

--- MyLesson3/Program.cs
+++ MyLesson3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int RevNum(int num)
+{
+int result = 0;
+while (num > 0) 
+    {
+        result *= 10;
+        result += num % 10;
+        num /= 10;
+    }
+return result;
+}
+
+bool IsPalindrom(int num)
+{
+  return num == RevNum(num); 
+}
+
+if (IsPalindrom(num))
+Console.Write(num + " - палиндром.");
+else
+Console.Write(num + " - не является палиндромом.")
+*/
